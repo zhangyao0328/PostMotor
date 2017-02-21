d941a1eaef08da7241dadae99ab41d9f482aefc6
zhangyao <zhangyao0328@gmail.com>
2017-02-21 12:11:29 +0800

    更新

--- app/src/main/java/com/postmotor/MainActivity.java
+++ app/src/main/java/com/postmotor/MainActivity.java
@@ -96,6 +96,8 @@ public class MainActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
         return true;
+
     }
 }
